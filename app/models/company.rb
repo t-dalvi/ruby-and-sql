069b715ae823e39b4baadadc5564d2935e005685
t-dalvi <tejas.dalvi@kellogg.northwestern.edu>
2026-02-03 15:53:17 +0000

    create tables

---
+++ app/models/company.rb
@@ -0,0 +1,3 @@
+class Company < ApplicationRecord
+  #Exists to talk to the companies table
+end
--- code-along/0-classes.rb
+++ code-along/0-classes.rb
@@ -2,7 +2,22 @@
 # when running this code.  To do so, your current working directory
 # should be the top-level directory (i.e. /workspace/your-app/) and then run:
 # rails runner code-along/0-classes.rb
-
+# puts "hi"
 # String Class
-
+bens_favorite_food = "tacos"
+tejas_favorite_food = "sushi"
 # String Methods
+
+puts bens_favorite_food.class
+puts tejas_favorite_food.class
+
+# puts String.methods
+
+class Dog
+  def speak
+    puts "Woof!"
+  end
+end
+
+rugs = Dog.new
+puts rugs.speak
